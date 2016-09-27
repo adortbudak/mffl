3b65413a4bea3e6a1179c7fa9f3043f7a73300cc
Alper Dortbudak <adortbudak@outlook.com>
2016-09-27 07:40:10 -0500

    fixed home page layout

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -9,18 +9,52 @@ export class HomePage {
   scheduleData:any
   constructor(public navCtrl: NavController) {
     this.scheduleData = [{
-      "date":"09/24/2016",
+      "date":"08/27/16",
       "time":"09:00 AM",
       "location": "W142 N8101 Merrimac Dr, Menomonee Falls, WI 53051",
-      "hometeam": "Indians-Maroon",
-      "awayteam": "WWG"
+      "hometeam": "Indians",
+      "opponent": "Waukesha North",
+      "vsat": "at"
     },
+      {
+        "date":"09/10/16",
+        "time":"09:00 AM",
+        "location": "20760 Hunters Run, Brookfield, WI 53045",
+        "opponent": "Sussex West",
+        "awayteam": "Indians",
+        "vsat": "at"
+      },
+      {
+        "date":"09/17/16",
+        "time":"10:00 AM",
+        "location": "20760 Hunters Run, Brookfield, WI 53045",
+        "opponent": "Catholic Memorial",
+        "awayteam": "Indians",
+        "vsat": "vs"
+      },
+      {
+        "date":"09/24/16",
+        "time":"09:00 AM",
+        "location": "W142 N8101 Merrimac Dr, Menomonee Falls, WI 53051",
+        "hometeam": "Indians",
+        "opponent": "Waukesha West Grey",
+        "vsat": "vs"
+      },
       {
         "date":"10/01/16",
         "time":"09:00 AM",
         "location": "20760 Hunters Run, Brookfield, WI 53045",
-        "hometeam": "SM",
-        "awayteam": "Indians-Maroon"
+        "opponent": "South Milwaukee",
+        "awayteam": "Indians",
+        "vsat": "at"
+      },
+      {
+        "date":"10/08/16",
+        "time":"10:00 AM",
+        "location": "20760 Hunters Run, Brookfield, WI 53045",
+        "opponent": "Kettle Moraine",
+        "awayteam": "Indians",
+        "vsat": "vs"
       }]
 
 
